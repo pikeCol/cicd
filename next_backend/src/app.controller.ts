fa525ba614f951f85295ff114f0c6e333d7bc66a
pikeCol <m13621613594@163.com>
2025-01-22 22:42:24 +0800

    gg

--- next_backend/src/app.controller.ts
+++ next_backend/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log('getHello 2211');
+    console.log('getHello 22131');
     return this.appService.getHello();
   }
 }
